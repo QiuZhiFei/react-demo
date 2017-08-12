ae0b810d45ded53c09270f2af49a5b7c54d89e52
qiuzhifei <qiuzhifei521@gmail.com>
2017-08-12 15:54:58 +0800

    f

--- home.js
+++ home.js
@@ -1,46 +1,60 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  NavigatorIOS
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    NavigatorIOS
 } from 'react-native';
 
+const onButtonPress= () => {
+        
+}
+
 export class Home extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button
+                    onPress={onButtonPress}
+                    title="Press Me"
+                    accessibilityLabel="See an informative alert"
+                />
+            </View>
+
+            //   <View style={styles.container}>
+            //     <Text style={styles.welcome}>
+            //       Welcome to React Native!
+            //     </Text>
+            //     <Text style={styles.instructions}>
+            //       To get started, edit index.ios.js
+            //     </Text>
+            //     <Text style={styles.instructions}>
+            //       Press Cmd+R to reload,{'\n'}
+            //       Cmd+D or shake for dev menu
+            //     </Text>
+            //   </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
